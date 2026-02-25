b799845a15614ed6ac41e14d09fb489411039663
SKadyrova <skadyrova@sportmasterlab.net>
2026-02-25 10:09:48 +0300

    correct bootstrap_servers

--- producers/message_producer.py
+++ producers/message_producer.py
@@ -16,9 +16,9 @@ logger = logging.getLogger()
 
  # Kafka брокеры
 BOOTSTRAP_SERVERS = [
-    'localhost:29092',
-    'localhost:29093',
-    'localhost:29094'
+    'localhost:9092',
+    'localhost:9093',
+    'localhost:9094'
 ]
 
 PRODUCER_CONFIG = {
